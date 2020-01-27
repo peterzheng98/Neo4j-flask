24391e96866ddb5212b5a5ce5f54a22c20c8addc
chesiy <49095156+chesiy@users.noreply.github.com>
2020-01-27 17:23:05 +0800

    now,I can use redirect!

--- app.py
+++ app.py
@@ -9,14 +9,21 @@ app = Flask(__name__)
 def homepage():
     if request.method == 'POST':
         symp_name = request.form.get('symptom_name')
-        print(symp_name)
-        drug_result = get_drug(symp_name)
-        print('*****************')
-        print(drug_result)
-        return render_template('suitable_drugs.html', drug_result=drug_result)
+
+        drug1_name =request.form.get('drug1')
+        drug2_name=request.form.get('drug2')
+        return redirect(url_for('getdrug',symp_name=symp_name))
 
     return render_template('home.html')
 
+@app.route('/drugs/?<string:symp_name>')
+def getdrug(symp_name):
+    print('here')
+    print(symp_name)
+    drug_result = get_drug(symp_name)
+    print('*****************')
+    print(drug_result)
+    return render_template('suitable_drugs.html', drug_result=drug_result)
 
 if __name__  ==  '__main__':
     app.run()
