e270cc0ddad40523c7b7e08f9d7593c40c6e5473
chesiy <49095156+chesiy@users.noreply.github.com>
2020-01-27 20:00:42 +0800

    now,I can use wtf-form....

--- app.py
+++ app.py
@@ -1,25 +1,38 @@
 from flask import Flask,render_template
 from flask import request,redirect,url_for
+from flask_wtf import FlaskForm
 from my_neo import get_drug
+from wtforms import StringField,SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "12345678"
 
+class symptomForm(FlaskForm):
+    sympt_name=StringField('Symptom:')
+    submit=SubmitField('Submit')
+
+class scriptForm(FlaskForm):
+    sympt_name=StringField('Symptom:')
+    drug1_name=StringField('Drug1:')
+    drug2_name=StringField('Drug2:')
+    submit=SubmitField('Submit')
 
 @app.route('/',methods=['POST','GET'])
 def homepage():
     if request.method == 'POST':
-        symp_name = request.form.get('symptom_name')
-
+        symp_name = request.form.get('sympt_name')
         drug1_name =request.form.get('drug1')
         drug2_name=request.form.get('drug2')
+
         return redirect(url_for('getdrug',symp_name=symp_name))
 
-    return render_template('home.html')
+    symptom_form=symptomForm()
+    script_form=scriptForm()
+    return render_template('home.html',symp_form=symptom_form,scri_form=script_form)
 
 @app.route('/drugs/?<string:symp_name>')
 def getdrug(symp_name):
-    print('here')
-    print(symp_name)
     drug_result = get_drug(symp_name)
     print('*****************')
     print(drug_result)
